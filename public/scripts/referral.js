fea750a79cfb1a763c4323ea0c84a9071775ca58
jung091232 <jcy091232@gmail.com>
2025-04-17 13:48:29 +0900

    Add files via upload

---
+++ public/scripts/referral.js
@@ -0,0 +1,45 @@
+
+let account;
+
+function connectWallet() {
+  if (window.ethereum) {
+    ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
+      account = accounts[0];
+      document.getElementById("wallet-address").innerText = "Wallet: " + account;
+      const refLink = "https://meverse.one/?ref=" + account;
+      document.getElementById("ref-link").value = refLink;
+      new QRCode(document.getElementById("qrcode"), {
+        text: refLink,
+        width: 128,
+        height: 128
+      });
+      simulateReferralData();
+    });
+  } else {
+    alert("Please install MetaMask!");
+  }
+}
+
+function copyLink() {
+  const link = document.getElementById("ref-link");
+  link.select();
+  link.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Referral link copied!");
+}
+
+function simulateReferralData() {
+  const count = Math.floor(Math.random() * 25) + 1;
+  const reward = count * 10;
+  document.getElementById("ref-count").innerText = count;
+  document.getElementById("ref-reward").innerText = reward;
+
+  const list = document.getElementById("ref-list");
+  list.innerHTML = "";
+  for (let i = 0; i < count; i++) {
+    const box = document.createElement("div");
+    box.className = "ref-box";
+    box.innerText = "0x..." + Math.random().toString(16).substr(2, 4).toUpperCase();
+    list.appendChild(box);
+  }
+}
